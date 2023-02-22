3bb5ff790863c0cb9eff80bd1a8107190f1021ea
gida hussami <g_hussami@hotmail.com>
2023-02-22 13:52:15 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+// colors:  {
+//   lorange: '#F3663F',
+//   gorange: '#F15025',
+//   dorange: '#E63D0F',
+//   bluencs: '#1d84b5',
+//   moonstoneblue: '#53A2BE',
+//   lavenderblush: '#FFEEF2',
+//   vistablue: '#8093F1',
+//   lav: '#B388EB'
+// }
